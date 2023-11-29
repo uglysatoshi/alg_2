de64adfc130c4f3adad86b2e2708363c40de7432
Satoshi <118019529+uglysatoshi@users.noreply.github.com>
2023-11-29 08:09:09 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/valery/projects/alg_2
-# Build directory: /home/valery/projects/alg_2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
